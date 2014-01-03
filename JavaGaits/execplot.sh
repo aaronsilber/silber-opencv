f6abbaa2b7d3c161c4204d4cebc179ca2e3a8e51
aaronsilber <aaron@aaronthesilber.com>
2014-01-03 17:37:09 -0500

    modified to graph computed means instead

--- JavaGaits/execplot.sh
+++ JavaGaits/execplot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/Desktop/testoutputs
-files=`ls *.csv | cat | sed 's/^/"/' | sed 's/$/" using 1:5 with lines lw 0.5 title columnhead/' | tr "\\n" ", "`
+files=`ls *mean.csv | cat | sed 's/^/"/' | sed 's/$/" using 1:2 with lines lw 3 title columnhead/' | tr "\\n" ", "`
 (cat<<__EOF
 set terminal wxt
 set datafile separator ","
