5d9331cdb579af71d2a2fb632cc506a6ff89f243
aaronsilber <aaron@aaronthesilber.com>
2014-01-03 14:35:10 -0500

    added shell script to plot generated data using gnuplot

---
+++ JavaGaits/execplot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~/Desktop/testoutputs
+files=`ls | cat | sed 's/^/"/' | sed 's/$/" using 1:5 with lines lw 0.5 title columnhead/' | tr "\\n" ", "`
+(cat<<__EOF
+set terminal wxt
+set datafile separator ","
+plot $files
+pause -1 "press enter to exit"
+__EOF
+read -r "enter") | gnuplot
